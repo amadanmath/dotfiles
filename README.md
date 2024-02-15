6eb1eb089201d5c18fe2f392200796f8445a5df5
Goran Topic <amadanmath+github@gmail.com>
2024-02-15 21:34:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My dotfiles
+
+These are my dotfiles, managed by [chezmoi](https://www.chezmoi.io/).
+Quickstart:
+
+```
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply amadanmath
+```
+
---
+++ dot_bash/os.Darwin.sh
@@ -0,0 +1,15 @@
+# VSCode
+[[ -x "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code" ]] &&
+  alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+
+# Movie players
+#alias vlc=/Applications/VLC.app/Contents/MacOS/VLC
+[[ -x "/Applications/DJV2.app/Contents/MacOS/DJV2" ]] &&
+  alias djv=/Applications/DJV2.app/Contents/MacOS/DJV2
+
+
+# Apple TTS
+alias sayko="say -v Yuna"
+alias sayjp="say -v Kyoko"
+alias saych="say -v Ting-Ting"
+
