7171db5d688e5f13fdf24768ca9e5bd443ac58ab
Goran Topic <amadanmath+github@gmail.com>
2024-02-16 11:12:31 +0900

    Use  for Python if available

--- dot_config/python/startup.py
+++ dot_config/python/startup.py
@@ -1,1 +1,12 @@
-from pprint import pp
+# Thanks to https://codeberg.org/scy/dotfiles/src/commit/467547d976cf09a67a7ca583185adb52d6978173/.config/python-repl-init.py
+
+import os
+import sys
+
+try:
+    from rich import pretty
+    pretty.install()
+    del pretty
+except Exception:
+    # Probably an ImportError, but we simply ignore any exception.
+    from pprint import pp
