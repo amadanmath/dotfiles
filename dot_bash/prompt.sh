bd55fccede9aab3e58cbcb5f81283d03732c1cda
Goran Topic <amadanmath+github@gmail.com>
2024-02-21 16:53:05 +0900

    Fixed the bug with PS1 and venv

--- dot_bash/prompt.sh
+++ dot_bash/prompt.sh
@@ -1,29 +1,27 @@
-ired="$(tput setaf 9)"
-igreen="$(tput setaf 10)"
-iyellow="$(tput setaf 11)"
-iblack="$(tput setaf 8)"
-off="$(tput sgr0)"
+__prompt_ired="$(tput setaf 9)"
+__prompt_igreen="$(tput setaf 10)"
+__prompt_iyellow="$(tput setaf 11)"
+__prompt_iblack="$(tput setaf 8)"
+__prompt_off="$(tput sgr0)"
 
 # See https://www.gnu.org/software/bash/manual/html_node/Controlling-the-Prompt.html
 __prompt_command() {
+  local soh=$'\x01'
+  local stx=$'\x02'
   local last_exit="$1"
-  local git_part=""
-  local prompt
-  local nested_prompt=""
-  local prompt_color
-  local i
+  local git_color
+
 
   if [ "$last_exit" -eq 0 ]
   then
-    prompt_color="$igreen"
+    __prompt_exit_color="$__prompt_igreen"
   else
-    prompt_color="$ired"
+    __prompt_exit_color="$ired"
   fi
 
   if git branch &>/dev/null
   then
     local branch="$(git symbolic-ref HEAD 2>/dev/null)"
-    local git_color
 
     if [ -n "$branch" ]; then
       branch=" (${branch##refs/heads/})"
@@ -31,13 +29,15 @@ __prompt_command() {
 
     if git diff-index --quiet HEAD 2>/dev/null
     then
-      git_color="$igreen"
+      git_color="$__prompt_igreen"
     else
-      git_color="$ired"
+      git_color="$__prompt_ired"
     fi
 
-    git_part="\[$git_color\]$branch\[$off\]"
+    __prompt_git="$soh$git_color$stx$branch$soh$__prompt_off$stx"
+  else
+    __prompt_git=""
   fi
-  export PS1="\[$iblack\]\h:\[$iyellow\]\w$git_part \[$prompt_color\]$SHLVL\[$iblack\]!\!\[$off\] \$ "
 }
 export PROMPT_COMMAND='__prompt_command $? '
+export PS1='\[$__prompt_iblack\]\h:\[$__prompt_iyellow\]\w$__prompt_git \[$__prompt_exit_color\]$SHLVL\[$__prompt_iblack\]!\!\[$__prompt_off\] \$ '
