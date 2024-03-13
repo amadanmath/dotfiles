a3270467707058d51d665f587428007b58772408
Goran Topic <amadanmath+github@gmail.com>
2024-03-13 18:57:10 +0900

    Modify Catppuccin with more visible WinSeparator

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local my_group = vim.api.nvim_create_augroup("my_group", { clear = true })
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "catppuccin",
+  group = mygroup,
+  command = "highlight WinSeparator guifg=#666666",
+})
