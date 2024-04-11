5f520a2f6d1efc47d0b14ff11ff7ebd870405976
Goran Topic <amadanmath+github@gmail.com>
2024-04-11 19:01:40 +0900

    Changed the nvim cmp keys: Tab confirms

--- private_dot_config/nvim/lua/plugins/luasnip.lua
+++ private_dot_config/nvim/lua/plugins/luasnip.lua
@@ -2,6 +2,9 @@ return {
   {
     "L3MON4D3/LuaSnip",
     lazy = false,
+    keys = function()
+      return {}
+    end,
     config = function()
       require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/lua/snippets" } })
     end,
--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,17 +1,37 @@
-local cmp = require("cmp")
-
 return {
   -- auto completion
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      mapping = {
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<S-CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = false,
-        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      },
-    },
+    -- Tried https://www.lazyvim.org/configuration/recipes#supertab
+    -- but those settings don't seem to fit me well
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- Confirm with Tab
+            cmp.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select   = true,
+            })
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.config.disable,
+      })
+    end,
   },
 }
