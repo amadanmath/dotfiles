99155cc0bffb7171193dd84eb8bd68a6303feb4e
Goran Topic <amadanmath+github@gmail.com>
2024-05-07 11:26:22 +0900

    Added Icecream import snippets

---
+++ private_dot_config/nvim/lua/snippets/ruby.lua
@@ -0,0 +1,10 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s({ trig = "qic", name = "Require Ricecream", desc = "Require Ricecream" }, {
+    t({ 'require "ricecream" # XXX: DEBUG: gem install ricecream', "" }),
+  }),
+}
+
