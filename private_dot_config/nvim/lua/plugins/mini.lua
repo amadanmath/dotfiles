945bf9b73c7204d7f55c59e6e0d0cd7b77fb8407
Goran Topic <amadanmath+github@gmail.com>
2024-06-14 17:58:24 +0900

    Add mini.bracketed

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,5 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
-  enabled = false, -- XXX: check if I want these
   config = function()
     -- Better Around/Inside textobjects
     --
@@ -8,7 +7,26 @@ return { -- Collection of various small independent plugins/modules
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+    require('mini.ai').setup {
+      n_lines = 500,
+      custom_textobjects = {
+        -- yiz. - a word including dots
+        z = function()
+          vim.api.nvim_echo({ { 'Word with character:' } }, false, {})
+          local char = vim.pesc(vim.fn.nr2char(vim.fn.getchar()))
+          return { '()()%f[%w_' .. char .. '][%w_' .. char .. ']+()[ \t]*()' }
+        end,
+        -- yiZ./<CR> - a word including dots and slashes
+        Z = function()
+          local char = vim.pesc(vim.fn.input 'Word with characters: ')
+          return { '()()%f[%w_' .. char .. '][%w_' .. char .. ']+()[ \t]*()' }
+        end,
+      },
+    }
+
+    require('mini.bracketed').setup {
+      quickfix = { suffix = '' }, -- use Trouble instead
+    }
 
     -- -- Add/delete/replace surroundings (brackets, quotes, etc.)
     -- --
