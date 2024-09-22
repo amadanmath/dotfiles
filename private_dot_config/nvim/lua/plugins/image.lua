59557643148f33bf819ecb72052d939161c73720
Goran Topic <amadanmath+github@gmail.com>
2024-09-22 11:34:52 +0900

    Disable image plugin on Windows terminal

--- private_dot_config/nvim/lua/plugins/image.lua
+++ private_dot_config/nvim/lua/plugins/image.lua
@@ -1,5 +1,6 @@
 -- Remember to install for libmagickwand-dev and ueberzugpp unless on kitty
 
+
 local backend = ''
 local enabled = true
 
@@ -7,6 +8,13 @@ if vim.env.TERM == 'xterm-kitty' then
   backend = 'kitty'
 elseif vim.fn.executable('ueberzug') then
   backend = 'ueberzug'
+  local uname = vim.fn.system('uname'):gsub('\n', '')
+  if uname == 'Linux' then
+    local lines = vim.fn.readfile('/proc/version')
+    if string.find(lines[1]:lower(), 'microsoft') then
+      enabled = false
+    end
+  end
 else
   enabled = false
 end
